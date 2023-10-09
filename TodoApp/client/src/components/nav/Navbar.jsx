32e82d867979ce179617eb5d9657b50594711265
Eric <eryk.ne05@gmail.com>
2023-10-09 19:06:45 +0200

    mobile view

--- TodoApp/client/src/components/nav/Navbar.jsx
+++ TodoApp/client/src/components/nav/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
 import Button from "./Button";
+
 const navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div className="flex h-16 items-center justify-around">
+    <div className="flex h-16 items-center justify-between border-b-2 border-white px-4 md:justify-around md:px-0">
       <h1 className="font-mono text-2xl font-semibold">TodoApp</h1>
-      <div className="flex gap-4">
+      <div className="hidden gap-4 md:flex">
         <Button name="Login" />
         <Button name="Register" special={true} />
       </div>
+      <div className="block md:hidden">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </div>
+      {isOpen ? (
+        <div className="absolute right-2 top-20 flex h-auto w-auto flex-col items-end justify-start gap-6 rounded-md bg-black/50 px-6 py-6">
+          <Button name="Login" />
+          <Button name="Register" special={true} />
+        </div>
+      ) : null}
     </div>
   );
 };
--- TodoApp/client/src/pages/Auth.jsx
+++ TodoApp/client/src/pages/Auth.jsx
@@ -7,9 +7,9 @@ import { Link } from "react-router-dom";
 export const Auth = () => {
   const [variant, setvariant] = useState("login");
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <div className="flex h-[550px] w-[55%] overflow-hidden rounded-md">
-        <div className="relative flex w-2/4 flex-col items-center justify-center bg-auth bg-cover bg-right-top text-white">
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex h-[550px] w-3/4 flex-col overflow-hidden rounded-md lg:w-[55%] lg:flex-row">
+        <div className="relative flex h-full w-full flex-col items-center justify-center bg-auth bg-cover bg-right-top text-white lg:w-2/4">
           <Link to={"/"}>
             <AiOutlineArrowLeft
               className="absolute left-4 top-4 z-10"
@@ -17,12 +17,12 @@ export const Auth = () => {
             />
           </Link>
           <h2 className="z-10 mb-2 text-5xl">To-Do App</h2>
-          <p className="z-10">
+          <p className="z-10 px-4">
             Discover, create & share lists of useful resources
           </p>
           <div className="absolute left-0 top-0 h-full w-full bg-black opacity-60"></div>
         </div>
-        <div className="flex h-full w-2/4 flex-col items-center justify-center gap-5 bg-[#171819]">
+        <div className="flex h-full w-full  flex-col items-center justify-center gap-5 bg-[#171819] lg:w-2/4">
           {variant == "login" ? (
             <Login
               onClick={() =>
