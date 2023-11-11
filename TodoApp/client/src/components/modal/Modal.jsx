4f205a5734d5f2f34ac88cf052b19a7e0da02a50
Eric <eryk.ne05@gmail.com>
2023-11-11 21:45:19 +0100

    Update Modal.jsx

--- TodoApp/client/src/components/modal/Modal.jsx
+++ TodoApp/client/src/components/modal/Modal.jsx
@@ -16,16 +16,24 @@ const Modal = ({ id, closeModal, updateData }) => {
     updateData();
   };
   return (
-    <div className=" absolute left-2/4 top-2/4 z-10 flex h-1/3 w-11/12  -translate-x-2/4 -translate-y-2/4 flex-col rounded-md border-2 border-white bg-black sm:h-64 sm:w-96">
-      <h2 className="border-b-2 border-white text-center">Edit task</h2>
-      <textarea
-        className="h-full resize-none bg-[#171819] px-2 text-white outline-none "
-        value={editTextValue}
-        onChange={(e) => setEditTextValue(e.target.value)}
-      />
-      <div className="flex justify-around border-t-2 border-white">
-        <button onClick={editTask}>Edit</button>
-        <button onClick={closeModal}>Cancel</button>
+    <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-black/50 backdrop-blur-md">
+      <div className=" flex h-1/3 w-11/12 flex-col rounded-md border-2 border-white bg-black sm:h-64 sm:w-96">
+        <h2 className="border-b-2 border-white text-center text-xl">
+          Edit task
+        </h2>
+        <textarea
+          className="h-full resize-none bg-[#171819] px-2 text-white outline-none "
+          value={editTextValue}
+          onChange={(e) => setEditTextValue(e.target.value)}
+        />
+        <div className="flex border-t-2 border-white">
+          <button onClick={editTask} className="h-full w-full">
+            Edit
+          </button>
+          <button onClick={closeModal} className="h-full w-full">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
