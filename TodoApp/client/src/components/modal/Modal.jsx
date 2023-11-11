097fb51c6112c89d2b38883a3cf83ddbb13cfadd
Eric <eryk.ne05@gmail.com>
2023-11-11 13:16:53 +0100

    Update Modal.jsx

--- TodoApp/client/src/components/modal/Modal.jsx
+++ TodoApp/client/src/components/modal/Modal.jsx
@@ -16,15 +16,14 @@ const Modal = ({ id, closeModal, updateData }) => {
     updateData();
   };
   return (
-    <div className=" absolute left-2/4 top-2/4 z-10 flex h-64 w-96 -translate-x-2/4 -translate-y-2/4 flex-col border-2 border-white bg-black">
-      <h2 className="text-center">Edit task</h2>
-      <input
-        type="text"
-        className="h-full text-start text-black"
+    <div className=" absolute left-2/4 top-2/4 z-10 flex h-1/3 w-11/12  -translate-x-2/4 -translate-y-2/4 flex-col rounded-md border-2 border-white bg-black sm:h-64 sm:w-96">
+      <h2 className="border-b-2 border-white text-center">Edit task</h2>
+      <textarea
+        className="h-full resize-none bg-[#171819] px-2 text-white outline-none "
         value={editTextValue}
         onChange={(e) => setEditTextValue(e.target.value)}
       />
-      <div className="flex justify-around">
+      <div className="flex justify-around border-t-2 border-white">
         <button onClick={editTask}>Edit</button>
         <button onClick={closeModal}>Cancel</button>
       </div>
